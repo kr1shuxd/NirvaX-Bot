c648849671a7e6ce5d1f0abb85d0472f649129a3
kr1shuxd <daddykrishu@gmail.com>
2024-10-28 00:00:43 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -36,7 +36,7 @@ module.exports = {
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              url: 'https://discord.gg/f2yvCFCcCW'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- YouTube - ${youtubeLink}`)
             .setImage('https://media.discordapp.net/attachments/1300060849888559176/1300164175821275136/standard.gif?ex=671fd7bf&is=671e863f&hm=8771b2551d0ff9c95ff8fccb63156a5ef2fb33a2382561b5c3ab76fd1335fe46&=&width=550&height=220')
