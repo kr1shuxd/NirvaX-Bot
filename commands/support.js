d8d682d3d7f71b6f3966de0c6b48e58f30b6ceb0
kr1shuxd <daddykrishu@gmail.com>
2024-10-27 21:37:50 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -29,12 +29,10 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://discord.gg/f2yvCFCcCW";
+      const youtubeLink = "https://www.youtube.com/@FyonixPlayz";
         const embed = new EmbedBuilder()
-            .setColor('#b300ff')
+            .setColor('#FB0808')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
