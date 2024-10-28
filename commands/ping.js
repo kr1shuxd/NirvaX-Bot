76eea7714698a84e7d1ccea56c335314574eeddb
kr1shuxd <daddykrishu@gmail.com>
2024-10-28 13:29:44 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -35,7 +35,7 @@ module.exports = {
       interaction.reply("Pinging....").then(msg => {
         const end = Date.now();
         const embed = new EmbedBuilder()
-          .setColor(config.embedColor)
+          .setColor('#ff0000')
           .setTitle(`Bot Latency`)
           .setDescription(`**Pong** : ${end - start}ms`)
         return interaction.editReply({ embeds: [embed] }).catch(e => { });
