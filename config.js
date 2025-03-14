923744d4ec8687fc63112fdb6d9816469ea4ba97
kr1shuxd <daddykrishu@gmail.com>
2025-03-14 21:33:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@
 */
 
 module.exports = {
-  TOKEN: "MTI5NzQ3NDcwNTg2NzE0NTI4Nw.GSVcRH.1CBIX7CDnM1Dt3uN0dkAXnXvTpXllHNsr30T9c",
+  TOKEN: "",
   ownerID: ["1282344443109838903", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
